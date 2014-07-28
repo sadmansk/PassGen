ec5686c8ee8804dbc44fbdcb39fba6757a3560ee
Sadman Kazi <sadmansazidk@gmail.com>
2014-07-28 15:03:02 -0400

    Added a read me file and wrote a dummy code

---
+++ PassGen.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <time.h>
+
+int main ()
+{
+	int size; //stores the size of the password
+	//prompt for the size;
+	std::cout << "Please enter the length of the password: ";
+	std::cin >> size;
+	//stores everything in an character array
+	char* pass = new Char[size];
+	for (int i = 30; i < 40; i++)
+	{
+		char [0] = i;
+		for (int j = 30; j < 40; j++)
+			...
+	}
+	return 0;
+}
+
+char* genPass (char* pass)
+
