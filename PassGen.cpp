9e94e0c6e040251f83a0be6747bd9dc039843e1e
Sadman Kazi <sadmansazidk@gmail.com>
2014-07-28 17:41:29 -0400

    Implemented the plans

--- PassGen.cpp
+++ PassGen.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <time.h>
 
+char* pass; //declares the password publicly so that its accessible independently from the recursive genPass method
+
+void genPass (int curSize)
+{
+	for (char i = '0'; i < '3'; i++)
+	{
+		pass [curSize - 1] = i;
+		if (curSize > 0) //the recursion continues as long as the current size (index) is greater than 0
+			genPass (curSize--); //sends in the previous index
+		else //stops the recursion when the password generation stops
+			std::cout << pass << std::endl; //displays the generated password when the each character is generated
+	}
+}
+
 int main ()
 {
 	int size; //stores the size of the password
-	//prompt for the size;
+	//prompt for the size
 	std::cout << "Please enter the length of the password: ";
 	std::cin >> size;
-	//stores everything in an character array
-	char* pass = new Char[size];
-	for (int i = 30; i < 40; i++)
-	{
-		char [0] = i;
-		for (int j = 30; j < 40; j++)
-			...
-	}
+	//sets the size of the character array;
+	pass = new char[size];
+
+	//generates the passwords and shows them on the screen
+	genPass (size);
 	return 0;
 }
-
-char* genPass (char* pass)
-
